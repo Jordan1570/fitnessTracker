5b90b1c532e392d20294feea46bfc6f8ce90799a
Jordan Scott <jscott0751@gmail.com>
2021-10-11 18:54:38 -0400

    completed server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+// const routes = require("./controllers")
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const User = require("./userModel.js");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
+
+// app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
