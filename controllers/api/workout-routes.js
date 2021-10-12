43c4bcc6d7606faaa3d27cb2f05c284c5a74f4d0
Jordan Scott <jscott0751@gmail.com>
2021-10-12 18:39:15 -0400

    edited workout route

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
+const Workout = require("../../models/Workout");
+
+router.get('/', (req, res) => {
+    Workout.find
+})
 
 module.exports = router;
--- models/Workout.js
+++ models/Workout.js
@@ -2,36 +2,46 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  type: {
-    type: String,
-    required: true
-  },
+  exercise: [
+    {
+      type: {
+        type: String,
+        required: true
+      },
+    
+      name: {
+        type: Number,
+        unique: true,
+        required: true
+      },
+    
+      duration: {
+        type: Number,
+        required: true
+      },
+    
+      weight: {
+        type: Number,
+        required: true
+      },
+    
+      reps: {
+        type: Number,
+        required: true
+      },
+    
+      sets: {
+        type: String,
+        required: true
+      }
+    }
+  ],
 
-  name: {
-    type: Number,
-    unique: true,
-    required: true
-  },
+  day: {
+    type: Date, 
+    default: Date.now
+  } 
 
-  duration: {
-    type: Number,
-    required: true
-  },
-
-  weight: {
-    type: Number,
-    required: true
-  },
-
-  reps: {
-    type: Number,
-    required: true
-  },
-
-  sets: {
-    type: String,
-    required: true
-  }
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
